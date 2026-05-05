984e9c4a1196e524bbb0eea658b85770c366a67d
LumisVal <leonid.gromov.2808@gmail.com>
2026-05-06 00:52:34 +0300

    Update confest.py

--- confest.py
+++ confest.py
@@ -1,27 +1,42 @@
+import os
 import pytest
+
 from selene import browser
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
     options = Options()
-    options.add_argument("--start-maximized")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-    service = Service(r"C:\drivers\chromedriver.exe")
-    driver = webdriver.Chrome(service=service, options=options)
+    remote_url = os.getenv("REMOTE_URL")
+
+    if remote_url:
+        options.set_capability("browserName", "chrome")
+        options.set_capability("browserVersion", os.getenv("BROWSER_VERSION", "128.0"))
+        options.set_capability(
+            "selenoid:options",
+            {
+                "enableVNC": True,
+                "enableVideo": True,
+                "name": "DemoQA tests"
+            }
+        )
+
+        driver = webdriver.Remote(
+            command_executor=remote_url,
+            options=options
+        )
+    else:
+        driver = webdriver.Chrome(options=options)
 
     browser.config.driver = driver
     browser.config.base_url = "https://demoqa.com"
     browser.config.timeout = 10
-
-    print("BASE URL =", browser.config.base_url)
-
-    yield
-
-    driver.quit()
 
     yield
 
